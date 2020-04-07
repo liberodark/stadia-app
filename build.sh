58eb55c338b2d1948087e677966629b48c5a38d7
liberodark <liberodark@gmail.com>
2020-04-07 12:57:31 +0200

    update

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# About: Build Stadia App automatically
+# Author: liberodark
+# Thanks : 
+# License: GNU GPLv3
+
+version="0.0.1"
+
+echo "Welcome on Stadia App Script $version"
+
+#=================================================
+# RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
+#=================================================
+
+distribution=$(cat /etc/*release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
+
+name="Stadia"
+arch="x64"
+dist="mac"
+electron="8.2.1"
+chrome="80.0.3987.132"
+
+# Check OS & nativefier
+
+ if ! command -v nativefier > /dev/null 2>&1; then
+  echo "Install nativefier ($distribution)"
+  npm install nativefier -g
+ fi
+
+# Fix ? app.allowRendererProcessReuse = true
+nativefier -e "$electron" -p "$dist" -a "$arch" -n "$name" \
+           --ignore-gpu-blacklist \
+           --single-instance \
+           -u "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$chrome Safari/537.36" \
+           --internal-urls ".*google\.com.*" \
+           "https://accounts.google.com/SignOutOptions?continue=https%3A%2F%2Fstadia.google.com%2F"
