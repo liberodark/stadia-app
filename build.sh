2d761418eeaac66fa4fbeb57ee12d665f1c29b16
liberodark <liberodark@gmail.com>
2020-04-08 11:56:51 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,8 +20,8 @@ electron="8.2.1"
 chrome="80.0.3987.163"
 
 npx nativefier -e "$electron" -p "$dist" -a "$arch" -n "$name" \
-           --ignore-gpu-blacklist \
-           --single-instance \
-           -u "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$chrome Safari/537.36" \
-           --internal-urls ".*google\.com.*" \
-           "https://accounts.google.com/SignOutOptions?continue=https%3A%2F%2Fstadia.google.com%2F"
+               --ignore-gpu-blacklist \
+               --single-instance \
+               -u "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$chrome Safari/537.36" \
+               --internal-urls ".*google\.com.*" \
+               "https://accounts.google.com/SignOutOptions?continue=https%3A%2F%2Fstadia.google.com%2F"
